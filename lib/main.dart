807288eb53f0a64fddebf718f527d7521fa7357e
NekoYasha <autornekoyasha@gmail.com>
2023-09-16 13:24:17 -0300

    Muda 'debugShowCheckedModeBanner' para 'false'

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: new Login());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: new Login(),);
   }
 }
