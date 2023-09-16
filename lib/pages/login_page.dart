ba9b67fc7dbce4bda386415d837df9da6cef1c9c
NekoYasha <autornekoyasha@gmail.com>
2023-09-16 13:24:47 -0300

    Cria base da página de login Criada página básica de login, que será aprimorada futuramente. 	Elementos da página base: 		- Campo de Texto para e-mail, 		- Campo de Text para senha, 		- Botão 'login'.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,28 +1,60 @@
 import "package:flutter/material.dart";
 
 class Login extends StatelessWidget {
+
   const Login({super.key});
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "e-mail",
+                      hintText: "insira um endereço de e-mail válido como example@gmail.com",
+                      prefixIcon: Icon(Icons.person),
+                    ),
                   ),
-                  child: const Text(
-                    "Login",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "senha",
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: Icon(Icons.remove_red_eye_sharp),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      minimumSize: const Size(double.infinity, 0),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(
+                        "entrar",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
                     ),
                   ),
                 )
