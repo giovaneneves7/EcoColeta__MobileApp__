c551676c348fc7710616b5fa63657003dbd262b1
NekoYasha <autornekoyasha@gmail.com>
2023-09-18 13:28:08 -0300

    Cria widget drawer para as páginas de usuário morador O widget será reaproveitado em páginas no escopo de morador.

--- lib/widgets/morador/morador_drawer.dart
+++ lib/widgets/morador/morador_drawer.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-
 class MoradorDrawer extends StatelessWidget {
   const MoradorDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
+
       child: SingleChildScrollView(
         child: Container(
           child: Column(
