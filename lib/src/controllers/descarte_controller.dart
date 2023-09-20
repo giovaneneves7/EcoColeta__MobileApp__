319a3786e5f47c38e8cfa38c3db9fd8707ad108e
NekoYasha <autornekoyasha@gmail.com>
2023-09-20 12:41:05 -0300

    Adiciona animação de câmera ao mudar de localização

--- lib/src/controllers/descarte_controller.dart
+++ lib/src/controllers/descarte_controller.dart
@@ -6,8 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class DescarteController extends GetxController {
-
-
   // ================================================ //
   //                     | ATTRIBUTES |               //
   // ================================================ //
@@ -23,6 +21,7 @@ class DescarteController extends GetxController {
   static DescarteController get to => Get.find<DescarteController>();
 
   get position => _position;
+
   get mapController => _mapsController;
 
   // ================================================ //
@@ -35,9 +34,7 @@ class DescarteController extends GetxController {
    * @param gmc - O GoogleMapController que será atribuido à variável '_mapsController'.
    */
   void onMapCreated(GoogleMapController gmc) async {
-
     _mapsController = gmc;
-
   }
 
   // ================================================ //
@@ -46,30 +43,22 @@ class DescarteController extends GetxController {
   /**
    * Acompanha a posição atual do usuário.
    */
-  void watchPosition() async{
-
+  void watchPosition() async {
     positionStream = Geolocator.getPositionStream().listen((position) {
-
-      if(position != null){
-
+      if (position != null) {
         latitude.value = position.latitude;
         longitude.value = position.longitude;
-
       }
-
     });
-
   }
 
   /**
    * Fecha o positionStream para evitar o uso desnecessário de memória.
    */
   @override
-  void onClose(){
-
+  void onClose() {
     positionStream.cancel();
     super.onClose();
-
   }
 
   /**
@@ -79,12 +68,12 @@ class DescarteController extends GetxController {
    * Pega a posição atual do usuário.
    */
   Future<Position> _currentPosition() async {
-
     LocationPermission permission;
     bool isActive = await Geolocator.isLocationServiceEnabled();
 
     if (!isActive) {
-      return Future.error("Por favor, habilite a localização no seu smartphone.");
+      return Future.error(
+          "Por favor, habilite a localização no seu smartphone.");
     }
 
     permission = await Geolocator.checkPermission();
@@ -108,23 +97,25 @@ class DescarteController extends GetxController {
    * Pega posição atual do usuário.
    */
   getPosition() async {
-
     try {
-
       final position = await _currentPosition();
 
       latitude.value = position.latitude;
       longitude.value = position.longitude;
 
+      _mapsController.animateCamera(
+        CameraUpdate.newLatLng(
+          LatLng(latitude.value, longitude.value),
+        ),
+      );
     } catch (e) {
       Get.snackbar(
-                    'Erro',
-                    e.toString(),
-                    backgroundColor: Colors.grey[900],
-                    colorText: Colors.white,
-                    snackPosition: SnackPosition.BOTTOM,
+        'Erro',
+        e.toString(),
+        backgroundColor: Colors.grey[900],
+        colorText: Colors.white,
+        snackPosition: SnackPosition.BOTTOM,
       );
     }
-
   }
 }
