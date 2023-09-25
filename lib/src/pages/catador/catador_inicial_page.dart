254da163339ef18da911f4dc738ac188099c34cf
NekoYasha <autornekoyasha@gmail.com>
2023-09-25 12:34:20 -0300

    Adiciona drawer à tela inicial

--- lib/src/pages/catador/catador_inicial_page.dart
+++ lib/src/pages/catador/catador_inicial_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ecocoleta/src/pages/catador/catador_descartes_page.dart';
 
 class CatadorInicialPage extends StatelessWidget {
   const CatadorInicialPage({super.key});
@@ -7,6 +8,106 @@ class CatadorInicialPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        drawer: Drawer(
+            child: SingleChildScrollView(
+              child: Container(
+                  child: Column( children: [
+                      Container(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: InkWell(
+                          child: const Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: Row(
+                              children: [
+                                Text("Descartes Novos"),
+                              ],
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(builder: (context) => CatadorDescartesPage()),
+                            );
+                          }
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 15.0),
+
+                        child: InkWell(
+                          onTap: () {},
+                          child: const Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: Row(
+                              children: [
+                                Text("Descartes Aceitos"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    Container(
+                      padding: const EdgeInsets.only(top: 15.0),
+
+                      child: InkWell(
+                        onTap: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.all(15.0),
+                          child: Row(
+                            children: [
+                              Text("Descartes Entregues"),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(top: 15.0),
+
+                      child: InkWell(
+                        onTap: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.all(15.0),
+                          child: Row(
+                            children: [
+                              Text("Minha Conta"),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(top: 15.0),
+
+                      child: InkWell(
+                        onTap: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.all(15.0),
+                          child: Row(
+                            children: [
+                              Text("Sair"),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    ],
+                  )
+              ),
+            )
+        ),
+        appBar: AppBar(
+          actions: const [
+            Row(
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.person,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
