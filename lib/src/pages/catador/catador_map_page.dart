10aaf332f0cb3d0ca9694d8375aa8dd215beec49
NekoYasha <autornekoyasha@gmail.com>
2023-09-25 12:32:30 -0300

    Adiciona evento 'onTap' aos marcadores

--- lib/src/pages/catador/catador_map_page.dart
+++ lib/src/pages/catador/catador_map_page.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:ecocoleta/src/controllers/descarte_controller.dart';
+import './catador_descarte_details_page.dart';
+import 'package:ecocoleta/src/widgets/customMarker.dart';
 
 class CatadorMapPage extends StatelessWidget {
-  const CatadorMapPage({super.key});
+  CatadorMapPage({super.key});
+
+  final descarteController = Get.put(DescarteController());
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: GetBuilder<DescarteController>(
+                  init: descarteController,
+                  builder: (value) => GoogleMap(
+                    mapType: MapType.normal,
+                    zoomControlsEnabled: true,
+                    initialCameraPosition: CameraPosition(
+                      target: descarteController.position,
+                      zoom: 13,
+                    ),
+                    onMapCreated: descarteController.onMapCreated,
+                    myLocationEnabled: true,
+                   /* onTap: (LatLng position) {
+                      Navigator.of(context!).push(
+                        MaterialPageRoute(
+                          builder: (context) => CatadorDescarteDetailsPage(),
+                        ),
+                      );
+                    },*/
+                    markers: Set<Marker>.from(descarteController.markers.map((marker) {
+                      return marker.copyWith(
+                        onTapParam: () {
+                          // Abra a nova página aqui quando o marcador for clicado
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => CatadorDescarteDetailsPage(name: marker.markerId.toString()),
+                            ),
+                          );
+                        },
+
+                      );
+                    })),
+                  )
+          ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
