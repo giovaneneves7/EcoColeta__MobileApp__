6dd55f0b0831f7893eb782c2c111f50699b17c4b
NekoYasha <autornekoyasha@gmail.com>
2023-09-26 13:28:45 -0300

    Adiciona chamada de testes para API

--- lib/src/pages/catador/catador_novos_descartes_page.dart
+++ lib/src/pages/catador/catador_novos_descartes_page.dart
@@ -1,20 +1,63 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:ecocoleta/src/widgets/novo_descarte.dart';
+import 'package:ecocoleta/src/models/descarte.dart';
 
 class CatadorNovosDescartesPage extends StatelessWidget {
-  const CatadorNovosDescartesPage({super.key});
+
+  CatadorNovosDescartesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Center(
-          child: Column(
-            children: [
-              const Text("NovosDescartes"),
-            ]
-          ),
+          child: FutureBuilder<List<Descarte>>(
+            future: getDescartes(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Erro: ${snapshot.error}');
+              } else {
+                List<NovoDescarte> descartesList = snapshot.data?.map((descarte) =>
+                    NovoDescarte(descarte: descarte,)
+                ).toList() ?? [];
+
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: descartesList,
+                );
+              }
+            },
+          )
         ),
       ),
     );
+  }
+
+  Future<List<Descarte>> getDescartes() async{
+
+    List<Descarte> descartes = [];
+    String url = "https://ecocoleta.free.beeceptor.com/descartes/novos";
+
+    http.Response response = await http.get(Uri.parse(url));
+
+    if(response.statusCode == 200){
+      print("Response: $response");
+      final List<dynamic> responseBody = json.decode(Utf8Codec().decode(response.bodyBytes));
+
+      for(var descarteJson in responseBody){
+
+        Descarte descarte = Descarte.fromJson(descarteJson);
+        descartes.add(descarte);
+
+      }
+    }
+
+    return descartes;
+
   }
 }
