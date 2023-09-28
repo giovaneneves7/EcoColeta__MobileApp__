902e8ff745eda30ab53336c33a9462c7233ec161
NekoYasha <autornekoyasha@gmail.com>
2023-09-28 13:11:58 -0300

    Adiciona evento de recusar descarte

--- lib/src/pages/catador/catador_novos_descartes_page.dart
+++ lib/src/pages/catador/catador_novos_descartes_page.dart
@@ -6,80 +6,86 @@ import 'package:ecocoleta/src/widgets/novo_descarte.dart';
 import 'package:ecocoleta/src/models/descarte.dart';
 
 class CatadorNovosDescartesPage extends StatefulWidget {
-
   CatadorNovosDescartesPage({super.key});
 
   @override
-  State<CatadorNovosDescartesPage> createState() => _CatadorNovosDescartesPageState();
+  State<CatadorNovosDescartesPage> createState() =>
+      _CatadorNovosDescartesPageState();
 }
 
 class _CatadorNovosDescartesPageState extends State<CatadorNovosDescartesPage> {
-
   List<NovoDescarte> descartesList = [];
 
-  void acceptDescarte(Descarte descarte){
-
-    setState((){
-      descartesList.removeWhere((item) => item.descarte.id == descarte.id);
+  void acceptDescarte(Descarte descarte) {
+    // Implementar lógica que atualizará o status do descarte para 'aceito'.
+    setState(() {
+      descartesList.removeWhere((descarteContainer) => descarteContainer.descarte.id == descarte.id);
     });
   }
+
+  void refuseDescarte(Descarte descarte) {
+    // Implementar lógica que recusará o descarte.
+    setState(() {
+      descartesList.removeWhere((descarteContainer) => descarteContainer.descarte.id == descarte.id);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FutureBuilder<List<Descarte>>(
-                future: getDescartes(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return CircularProgressIndicator();
-                  } else if (snapshot.hasError) {
-                    return Text('Erro: ${snapshot.error}');
-                  } else {
-                    List<NovoDescarte> descartesList = snapshot.data?.map((descarte) =>
-                        NovoDescarte(descarte: descarte,onAccept: () => acceptDescarte(descarte),)
-                    ).toList() ?? [];
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FutureBuilder<List<Descarte>>(
+              future: getDescartes(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Erro: ${snapshot.error}');
+                } else {
+                  List<NovoDescarte> descartesList = snapshot.data
+                          ?.map((descarte) => NovoDescarte(
+                                descarte: descarte,
+                                onAccept: () => acceptDescarte(descarte),
+                                onRefuse: () => refuseDescarte(descarte),
+                              ))
+                          .toList() ?? [];
 
-                    return Flexible(
-                      child: ListView(
-                        shrinkWrap: true,
-                        children: descartesList,
-                      ),
-
-                    );
-                  }
-                },
-              ),
-            ],
-          )
-        ),
+                  return Flexible(
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: descartesList,
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
+        )),
       ),
     );
   }
 
-  Future<List<Descarte>> getDescartes() async{
-
+  Future<List<Descarte>> getDescartes() async {
     List<Descarte> descartes = [];
     String url = "https://ecocoleta.free.beeceptor.com/descartes/novos";
 
     http.Response response = await http.get(Uri.parse(url));
 
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       print("Response: $response");
-      final List<dynamic> responseBody = json.decode(Utf8Codec().decode(response.bodyBytes));
+      final List<dynamic> responseBody =
+          json.decode(Utf8Codec().decode(response.bodyBytes));
 
-      for(var descarteJson in responseBody){
-
+      for (var descarteJson in responseBody) {
         Descarte descarte = Descarte.fromJson(descarteJson);
         descartes.add(descarte);
-
       }
     }
 
     return descartes;
-
   }
 }
