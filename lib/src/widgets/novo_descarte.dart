fe3ef9ecc9abe9d3d57639b47faf9d582e1a95ad
NekoYasha <autornekoyasha@gmail.com>
2023-09-28 13:35:01 -0300

    Edita estilo do widget

--- lib/src/widgets/novo_descarte.dart
+++ lib/src/widgets/novo_descarte.dart
@@ -13,6 +13,8 @@ class NovoDescarte extends StatefulWidget {
   final VoidCallback onAccept;
   final VoidCallback onRefuse;
 
+  static const double SIZED_BOX_WIDTH = 6.0;
+
   @override
   State<NovoDescarte> createState() => _NovoDescarteState();
 }
@@ -20,86 +22,97 @@ class NovoDescarte extends StatefulWidget {
 class _NovoDescarteState extends State<NovoDescarte> {
   @override
   Widget build(BuildContext context) {
+
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(4),
         color: Colors.green[300],
       ),
+
       margin: const EdgeInsets.all(2.0),
       padding: const EdgeInsets.all(8.0),
-
       alignment: Alignment.center,
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Text(
-                widget.descarte.id.toString(),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Text(
-                widget.descarte.material,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                    onPressed: () {
 
-                      widget.onAccept();
+      child: Center(
+        child: Column(
 
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                    ),
-                    child: const Text(
-                      "Aceitar",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 4.0,),
-                  TextButton(
-                    onPressed: () {
-                      widget.onRefuse();
-                    },
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.black),
-                    ),
-                    child: const Text(
-                      "Detalhes",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 4.0,),
-                  TextButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.black),
-                    ),
-                    child: const Text(
-                      "Recusar",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text(
+                  widget.descarte.id.toString(),
+                ),
               ),
-            ),
-          ]),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text(
+                  widget.descarte.material,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+
+                        widget.onAccept();
+
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                      ),
+                      child: const Text(
+                        "Aceitar",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(width: NovoDescarte.SIZED_BOX_WIDTH,),
+
+                    TextButton(
+                      onPressed: () {
+                        widget.onRefuse();
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.black),
+                      ),
+                      child: const Text(
+                        "Detalhes",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(width: NovoDescarte.SIZED_BOX_WIDTH,),
+
+                    TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.black),
+                      ),
+                      child: const Text(
+                        "Recusar",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+      ),
     );
   }
 }
