856e660ef8c1bb20ad11128fa25c78cde74f2735
NekoYasha <autornekoyasha@gmail.com>
2023-10-02 09:51:30 -0300

    Cria classe de serviço para o login

---
+++ lib/src/services/login_service.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class LoginService{
+
+
+  Future<bool> isValidUser({required String email, required String password}) async{
+
+    String url = "http://localhost:8080/catador/getAll";
+
+    http.Response response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+
+      print("Response: $response");
+      final responseBody = json.decode(response.body);
+      print("ResponseBody: $responseBody");
+
+      for(var item in responseBody){
+
+        if(email == item['email'] && password == item['senha']){
+          return true;
+        }
+      }
+
+    }
+
+
+    return false;
+  }
+
+}
