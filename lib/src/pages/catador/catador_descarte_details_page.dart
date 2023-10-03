6d8e591b85fa5dda20b65699c95f442a34e00ee1
NekoYasha <autornekoyasha@gmail.com>
2023-10-03 10:09:11 -0300

    Adiciona requisição de testes para carregar informações

--- lib/src/pages/catador/catador_descarte_details_page.dart
+++ lib/src/pages/catador/catador_descarte_details_page.dart
@@ -1,10 +1,27 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class CatadorDescarteDetailsPage extends StatelessWidget {
+class CatadorDescarteDetailsPage extends StatefulWidget {
 
-  CatadorDescarteDetailsPage({super.key, required this.name});
+  CatadorDescarteDetailsPage({super.key, required this.descarteId});
 
-  String name;
+  final int descarteId;
+
+  @override
+  State<CatadorDescarteDetailsPage> createState() => _CatadorDescarteDetailsPageState();
+}
+
+class _CatadorDescarteDetailsPageState extends State<CatadorDescarteDetailsPage> {
+  Map<String, dynamic> descarteInfos = {};
+
+  @override
+  void initState() {
+    super.initState();
+    getInfos();
+  }
+  
+
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +33,7 @@ class CatadorDescarteDetailsPage extends StatelessWidget {
               child: Column(
 
                 children: [
-                  const Text("Endereço:"),
-                  Text("Vila Boa Esperança."),
+                  Text("${descarteInfos['id'] ?? ''}"),
                 ],
               ),
 
@@ -26,5 +42,22 @@ class CatadorDescarteDetailsPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<List<String>> getInfos() async{
+
+    final String url = "http://localhost:8080/catador/get/${widget.descarteId}";
+    http.Response response = await http.get(Uri.parse(url));
+
+
+    if (response.statusCode == 200) {
+
+      print("Response: $response");
+      final responseBody = json.decode(response.body);
+      print("ResponseBody: $responseBody");
+
+      infos['latitude'] = responseBody['latitude'];
+
+    }
   }
 }
