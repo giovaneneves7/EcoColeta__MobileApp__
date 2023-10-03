f43b9ba982692f295ea178cd70fe94649ecf2695
NekoYasha <autornekoyasha@gmail.com>
2023-10-03 09:44:52 -0300

    Adiciona novos atributos à classe

--- lib/src/models/descarte.dart
+++ lib/src/models/descarte.dart
@@ -1,17 +1,18 @@
-import 'package:ecocoleta/src/models/position.dart';
-import 'package:geolocator/geolocator.dart';
 
 class Descarte{
 
-  Descarte({required this.id, required this.material,});
+  Descarte({required this.id, required this.material, required this.quantidade, required this.latitude, required this.longitude});
 
   int id;
   String material;
-  double? address;
+  double? valor;
+  double quantidade;
+  double latitude;
+  double longitude;
 
   factory Descarte.fromJson(Map<String, dynamic> json) {
 
-    return Descarte(id: json['id'], material: json['material']);
+    return Descarte(id: json['id'], material: json['material'], quantidade: json['quantidade'], latitude: json['latitude'], longitude: json['longitude'],);
 
   }
 
