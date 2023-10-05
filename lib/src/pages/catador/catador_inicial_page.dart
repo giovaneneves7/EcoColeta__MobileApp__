09eb69b272cdb6f086b8e49f3df439d94501392a
NekoYasha <autornekoyasha@gmail.com>
2023-10-05 11:09:36 -0300

    Edita estilo da página

--- lib/src/pages/catador/catador_inicial_page.dart
+++ lib/src/pages/catador/catador_inicial_page.dart
@@ -12,97 +12,93 @@ class CatadorInicialPage extends StatelessWidget {
       child: Scaffold(
         drawer: Drawer(
             child: SingleChildScrollView(
-              child: Container(
-                  child: Column( children: [
-                      Container(
-                        padding: const EdgeInsets.only(top: 15.0),
-                        child: InkWell(
-                          child: const Padding(
-                            padding: EdgeInsets.all(15.0),
-                            child: Row(
-                              children: [
-                                Text("Descartes Novos"),
-                              ],
-                            ),
-                          ),
-                          onTap: () {
-                            Navigator.of(context).push(
-                                MaterialPageRoute(builder: (context) => CatadorDescartesPage()),
-                            );
-                          }
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.only(top: 15.0),
-
-                        child: InkWell(
-                          onTap: () {},
-                          child: const Padding(
-                            padding: EdgeInsets.all(15.0),
-                            child: Row(
-                              children: [
-                                Text("Descartes Aceitos"),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    Container(
-                      padding: const EdgeInsets.only(top: 15.0),
-
-                      child: InkWell(
-                        onTap: () {},
-                        child: const Padding(
-                          padding: EdgeInsets.all(15.0),
-                          child: Row(
-                            children: [
-                              Text("Descartes Entregues"),
-                            ],
-                          ),
-                        ),
+          child: Container(
+              child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: InkWell(
+                    child: const Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Row(
+                        children: [
+                          Text("Descartes Novos"),
+                        ],
                       ),
                     ),
-                    Container(
-                      padding: const EdgeInsets.only(top: 15.0),
-
-                      child: InkWell(
-                        onTap: () {},
-                        child: const Padding(
-                          padding: EdgeInsets.all(15.0),
-                          child: Row(
-                            children: [
-                              Text("Minha Conta"),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(top: 15.0),
-
-                      child: InkWell(
-                        onTap: () {},
-                        child: const Padding(
-                          padding: EdgeInsets.all(15.0),
-                          child: Row(
-                            children: [
-                              Text("Sair"),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    ],
-                  )
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (context) => CatadorDescartesPage()),
+                      );
+                    }),
               ),
-            )
-        ),
+              Container(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: InkWell(
+                  onTap: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Row(
+                      children: [
+                        Text("Descartes Aceitos"),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: InkWell(
+                  onTap: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Row(
+                      children: [
+                        Text("Descartes Entregues"),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: InkWell(
+                  onTap: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Row(
+                      children: [
+                        Text("Minha Conta"),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: InkWell(
+                  onTap: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Row(
+                      children: [
+                        Text("Sair"),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )),
+        )),
         appBar: AppBar(
           actions: [
             IconButton(
               icon: const Icon(Icons.person),
               onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => UserSettingsPage()));
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => UserSettingsPage()));
               },
             ),
           ],
@@ -110,17 +106,39 @@ class CatadorInicialPage extends StatelessWidget {
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Column(children: [
-              Container(
-                color: const Color(0xFF03860AFF),
-                child: Column(
-                  children: const [
-                    Text("Coleta Seletiva"),
-                    Text("é a maneira ecológica mais adequada para o descarte.")
-                  ],
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFF71be78),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5.0,
+                        blurRadius: 7.0,
+                        offset: Offset(0, 3.0),
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: const [
+                        Text(
+                          "Coleta Seletiva",
+                          style: TextStyle(color: Colors.white, fontSize: 18.0),
+                        ),
+                        Text(
+                          "é a maneira ecológica mais adequada para o descarte.",
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-            ]),
+              ],
+            ),
           ),
         ),
       ),
