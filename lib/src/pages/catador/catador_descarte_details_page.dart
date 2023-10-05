b2fb37e53271cd7b04a6ac2f34db1f4368491ad9
NekoYasha <autornekoyasha@gmail.com>
2023-10-05 10:20:08 -0300

    cria método 'getAddress'

--- lib/src/pages/catador/catador_descarte_details_page.dart
+++ lib/src/pages/catador/catador_descarte_details_page.dart
@@ -1,70 +1,83 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:http/http.dart' as http;
 
 class CatadorDescarteDetailsPage extends StatefulWidget {
-
   CatadorDescarteDetailsPage({super.key, required this.descarteId});
 
   final int descarteId;
 
   @override
-  State<CatadorDescarteDetailsPage> createState() => _CatadorDescarteDetailsPageState();
+  State<CatadorDescarteDetailsPage> createState() =>
+      _CatadorDescarteDetailsPageState();
 }
 
-class _CatadorDescarteDetailsPageState extends State<CatadorDescarteDetailsPage> {
+class _CatadorDescarteDetailsPageState
+    extends State<CatadorDescarteDetailsPage> {
   Map<String, dynamic> descarteInfos = {};
+  String address = "";
 
   @override
   void initState() {
     super.initState();
     mockGetInfos();
   }
-  
-
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              child: Column(
-
-                children: [
-                  Text("ID: ${descarteInfos['id'] ?? ''}"),
-                  Text("Material: ${descarteInfos['tipoMaterial'] ?? ''}"),
-                ],
-              ),
-
+        body: Column(children: [
+          Expanded(
+            child: Column(
+              children: [
+                const Text(
+                  "Endereço:",
+                  style: TextStyle(fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  address,
+                ),
+                Text("ID: ${descarteInfos['id'] ?? ''}"),
+                const Text(
+                  "Observações",
+                  style: TextStyle(fontWeight: FontWeight.w700),
+                ),
+                const Text(
+                  "Tipo de Material:",
+                  style: TextStyle(fontWeight: FontWeight.w700),
+                ),
+                Text("Material: ${descarteInfos['tipoMaterial'] ?? ''}"),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Chat Morador"),
+                )
+              ],
             ),
-          ]
-        ),
+          ),
+        ]),
       ),
     );
   }
 
-  Future<void> getInfos() async{
-
+  Future<void> getInfos() async {
     final String url = "http://localhost:8080/catador/get/${widget.descarteId}";
     http.Response response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
-
       print("Response: $response");
       final responseBody = json.decode(response.body);
       print("ResponseBody: $responseBody");
 
-      setState((){
+      setState(() {
         descarteInfos = responseBody;
       });
-
     }
   }
 
-  Future<void> mockGetInfos() async{
-
+  Future<void> mockGetInfos() async {
     Map<String, dynamic> infos = {};
 
     infos.addAll({
@@ -74,9 +87,32 @@ class _CatadorDescarteDetailsPageState extends State<CatadorDescarteDetailsPage>
       'id': 1,
     });
 
-    setState((){
+    setState(() {
       descarteInfos = infos;
     });
 
+    await getAddress(
+      infos['latitude'],
+      infos['longitude'],
+    );
+  }
+
+  Future<void> getAddress(double latitude, double longitude) async {
+    try {
+      List<Placemark> placemarks =
+          await placemarkFromCoordinates(latitude, longitude);
+
+      if (placemarks.isNotEmpty) {
+        Placemark placemark = placemarks[0];
+        String fullAddress =
+            "${placemark.street}, ${placemark.subLocality}, ${placemark.locality}";
+
+        setState(() {
+          address = fullAddress;
+        });
+      }
+    } catch (e) {
+      print("Erro ao obter o endereço, verifique as coordenadas: $e");
+    }
   }
 }
