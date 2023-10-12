d11538d28ab744aed8a90f2497ca9a8b61eb39c5
NekoYasha <autornekoyasha@gmail.com>
2023-10-12 11:24:12 -0300

    📜 feat: Cria página de registro de usuários

---
+++ lib/src/pages/registro_page.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+class RegistroPage extends StatefulWidget {
+  RegistroPage({super.key});
+
+  @override
+  State<RegistroPage> createState() => _RegistroPageState();
+}
+
+class _RegistroPageState extends State<RegistroPage> {
+  TextEditingController emailController = new TextEditingController();
+
+  TextEditingController passwordController = new TextEditingController();
+
+  String? selectedOption = 'Catador';
+
+  void handleRadioValueChanged(String? newValue) {
+    setState(() {
+      selectedOption = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+            child: Column(
+              children: [
+                Text(
+                  "Registrar",
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w700, color: Colors.green[700],),
+                ),
+                const SizedBox(height: 8.0),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6.0),
+                      ),
+                      labelText: "digite o seu nome",
+                      hintText: "Insira um nome válido como 'João da Silva'",
+                      prefixIcon: const Icon(Icons.person),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6.0),
+                      ),
+                      labelText: "digite o e-mail",
+                      hintText:
+                          "insira um endereço de e-mail válido como example@gmail.com",
+                      prefixIcon: const Icon(Icons.mail),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      labelText: "digite a senha senha",
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: const Icon(Icons.remove_red_eye_sharp),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      labelText: "digite a senha novamente",
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: const Icon(Icons.remove_red_eye_sharp),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: RadioListTile(
+                          title: Text('Catador'),
+                          value: 'catador',
+                          groupValue: selectedOption,
+                          onChanged: handleRadioValueChanged,
+                        ),
+                      ),
+                      Expanded(
+                        child: RadioListTile(
+                          title: Text('Morador'),
+                          value: 'morador',
+                          groupValue: selectedOption,
+                          onChanged: handleRadioValueChanged,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    minimumSize: const Size(double.infinity, 0),
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      "registrar",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
