15d03d0462cc09a362ab77ebeb2c27e0bde9ba56
giovaneneves7 <autornekoyasha@gmail.com>
2024-05-04 14:34:16 -0300

    feat (splash): Adiciona GetBuilder de SplashController

--- lib/features/splash/controllers/splash_controller.dart
+++ lib/features/splash/controllers/splash_controller.dart
@@ -3,4 +3,10 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashController extends GetxController implements GetxService{
 
+	bool _firstTimeConnectionCheck = true;
+	bool get firstTimeConnectionCheck => _firstTimeConnectionCheck;
+
+	bool _hasConnection = true;
+	bool get hasConnection => _hasConnection;
+	
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:ecocoleta/common/controllers/theme_controller.dart';
+import 'package:ecocoleta/theme/dark_theme.dart';
+import 'package:ecocoleta/theme/light_theme.dart';
 import 'package:ecocoleta/features/splash/controllers/splash_controller.dart';
 import 'package:ecocoleta/util/app_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:ecocoleta/helper/get_di.dart' as di;
 import 'package:get/get.dart';
+import 'package:ecocoleta/helper/route_helper.dart';
 
 Future<void> main() async {
 
@@ -39,9 +43,35 @@ class _MyAppState extends State<MyApp> {
           navigatorKey: Get.key,
           theme: themeController.darkTheme ? dark() : light(),
           // locale: /* TODO: Create a localizeController */
-          translations: Messages(languages: widget.languages),
+          //translations: Messages(languages: widget.languages),
+          initialRoute: RouteHelper.getInitialRoute(), /*TODO: Adicionar rota splash*/
+          getPages: RouteHelper.routes,
+          defaultTransition: Transition.topLevel,
+          transitionDuration: const Duration(milliseconds: 500),
+          builder: (BuildContext context, widget){
 
-        );
+            return MediaQuery(
+
+              data: MediaQuery.of(context).copyWith(textScaler: const TextScaler.linear(1)), 
+              child: Material(
+
+                child: Stack(
+                
+                  children: [
+                    
+                    widget!,
+                    GetBuilder<SplashController>(builder: (SplashController){
+
+                      return const SizedBox();
+
+                    }),
+
+                  ],
+                ),
+              ),
+            );
+
+          });
 
       });
 
